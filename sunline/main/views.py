b023f9ea95ae4c8111b2ea6ebf15ee317667dfa2
TimWolfskin <timwolfskin013@gmail.com>
2024-01-05 00:13:38 +0300

    added fixtures for goods and links for routes products

--- sunline/main/views.py
+++ sunline/main/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from goods.models import Categories
 
 def index(request):
+
+    categories = Categories.objects.all()
+
     context = {
         'title': 'Sunline - main page',
-        'content': 'furniture ecommerce Sunline'
+        'content': 'furniture ecommerce Sunline',
+        'categories': categories
     }
     return render(request, "main/index.html", context)
 
