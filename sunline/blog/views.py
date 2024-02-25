49b0d0e3637ed700435497c3f38abd4f55bdd833
TimWolfskin <timwolfskin013@gmail.com>
2024-02-26 01:27:57 +0300

    Added models for blog

--- sunline/blog/views.py
+++ sunline/blog/views.py
@@ -1,7 +1,14 @@
+from urllib import request
 from django.shortcuts import render
 from django.http import HttpResponse
+from blog.models import Post
 
 
 def blog(request):
-    return render(request, "blog/blog.html")
+    post_list = Post.objects.all()
+
+    context = {
+        "post_list": post_list
+    }
+    return render(request, "blog/blog.html", context)
 
--- sunline/sunline/settings.py
+++ sunline/sunline/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     "django.contrib.postgres",
 
     "debug_toolbar",
+    "mptt",
+
     "main",
     "goods",
     "users",
